f5a9d46a7c19f46e80102eb30748453e09525317
bellkimkeith <157334752+bellkimkeith@users.noreply.github.com>
2024-06-02 21:24:57 +0800

    added meetingsetup modal to redirect in meeting room

---
+++ components/MeetingSetup.tsx
@@ -0,0 +1,61 @@
+"use client";
+import {
+  //   DeviceSettings,
+  VideoPreview,
+  useCall,
+} from "@stream-io/video-react-sdk";
+import { list } from "postcss";
+import React, { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+
+const MeetingSetup = ({
+  setIsSetupComplete,
+}: {
+  setIsSetupComplete: (value: boolean) => void;
+}) => {
+  const [isMicCamToggleOn, setIsMicCamToggleOn] = useState(false);
+  const call = useCall();
+
+  if (!call) {
+    throw new Error("useCall must be withtin StreamCall component");
+  }
+
+  useEffect(() => {
+    if (isMicCamToggleOn) {
+      call.camera.disable();
+      call.microphone.disable();
+    } else {
+      call.camera.enable();
+      call.microphone.enable();
+    }
+  }, [isMicCamToggleOn, call.camera, call.microphone]);
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-3 text-white h-screen w-full">
+      <h1 className="text-2xl font-semibold">Setup</h1>
+      <VideoPreview />
+      <div className="flex h-16 justify-center items-center gap-3">
+        <label className="flex items-center justify-center gap-2 font-medium">
+          <input
+            type="checkbox"
+            checked={isMicCamToggleOn}
+            onChange={(e) => setIsMicCamToggleOn(e.target.checked)}
+          />
+          Join with mic and camera off
+        </label>
+        {/* <DeviceSettings /> */}
+      </div>
+      <Button
+        className="rounded-md bg-green-500 px-4 py-2.5 hover:bg-green-800"
+        onClick={() => {
+          call.join();
+          setIsSetupComplete(true);
+        }}
+      >
+        Join meeting
+      </Button>
+    </div>
+  );
+};
+
+export default MeetingSetup;
